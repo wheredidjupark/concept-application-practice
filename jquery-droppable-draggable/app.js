acd5a1289a49cd08b8c0a35d6cf65fcb01544223
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-11-26 11:40:53 -0800

    jquery-dorppable experiment

--- jquery-droppable-draggable/app.js
+++ jquery-droppable-draggable/app.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 	$("#trashcan").droppable({
 		drop: function(){
 			console.log('dropped!');
-		}
+		},
+		acceptable: "#item"
 	});
 
 	$("#item").draggable();
+	$("#item2").draggable();
 });
