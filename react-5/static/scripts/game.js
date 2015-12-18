be42e85e9e727fa2c5d0127ec73275293fa955ef
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-12-18 10:30:28 -0800

    adding number of stars

--- react-5/static/scripts/game.js
+++ react-5/static/scripts/game.js
@@ -1,10 +1,18 @@
 var StarFrame = React.createClass({
   render: function(){
 
+    var numberOfStars = 5;
+    var stars = [];
+    for(var i=0; i < numberOfStars; i++){
+      stars.push(
+        <span className="glyphicon glyphicon-star"></span>
+        );
+    }
+
     return (
       <div id="stars-frame">
         <div className ="well">
-          <span className="glyphicon glyphicon-star"></span>
+          {stars}
         </div>
       </div>
       );
